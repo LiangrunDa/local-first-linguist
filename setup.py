baf84b9ef685cd7863e631452ef9a0c8148ec3a8
LiangrunDa <liangrun_da@outlook.com>
2023-11-18 23:51:17 +0100

    version 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='local-first-linguist',
-    version='0.1',
+    version='0.2.1',
     author_email='me@liangrunda.com',
     license='MIT',
     packages=find_packages(),
